87b65120214adbd23866fb6a7dad9c07ed9ccc31
Sparky <sparky0904@gmail.com>
2014-06-27 11:52:55 +0100

    Wokring version, copys files only

--- Archiver/Program.cs
+++ Archiver/Program.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.Win32;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Archiver
 {
+
+    /* 
+     * Verison History
+     * 
+     * Jun-14: Inital program created
+     * 
+     * - Copys files only
+     * - does not insert registry entry this has to be done manually
+     * 
+    */
+
     static class Program
     {
-        
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -24,13 +29,27 @@ namespace Archiver
             clsRegistry Registry;
             Registry = new clsRegistry();
 
+            clsTools myTools = new clsTools();
+
             clsArchiverTools archiverTools;
             archiverTools = new clsArchiverTools();
+
+            string fileName = args[0].ToString();
+
+            //TODO: Add code to verify if Archive settings are in registry
+
+            // check Registry
+            // is the registry entry the same as the current program path!
+
+            // If not valid enter new settings
+
+            // End check
 
             //Registry.addMenu_Click("Archiver", "cmd");
             //Registry.btnRemoveMenu_Click("Archiver", "cmd");
 
-            MessageBox.Show(args[0].ToString());
+            //MessageBox.Show(fileName,myTools.PublishVersion);
+            archiverTools.copyFile(fileName);
         }
     }
 }
--- Archiver/clsArchiverTools.cs
+++ Archiver/clsArchiverTools.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Archiver
@@ -10,17 +7,38 @@ namespace Archiver
     class clsArchiverTools
     {
         public string errorMessage { get; set; }
+
         public void copyFile(string fileName)
         {
             try
             {
-                DateTime todaysDate;
-                //todaysDate = now();
+                string todaysDate;
+                string newFileName;
+                
+                // Seperate filename into filename and extension
+                string[] filenameParts = fileName.Split('.');                
+
+                // get new timestamp
+                todaysDate = "-" + DateTime.Now.ToString("yyMMdd") + "-" + DateTime.Now.ToString("hhmm");
+
+                // Add todays timestamp onto the end of the file name to get new file name
+                newFileName = filenameParts[0] + todaysDate;
+
+                // check if there is an extension
+                if (filenameParts.Length > 1)
+                {
+                    newFileName += "." + filenameParts[1];
+                }
+
+                //MessageBox.Show(newFileName, "New File name");
+
+                // copy the file
+                File.Copy(fileName, newFileName);
             }
             catch (Exception ex)
             {
                 errorMessage = ex.ToString();
-                MessageBox.Show(errorMessage, "Error when copying file.", MessageBoxButtons.OK);
+                MessageBox.Show(errorMessage, "Error copying file.", MessageBoxButtons.OK);
                 throw;
             }
         }
