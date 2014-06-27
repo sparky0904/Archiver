c1c5ddbf6ff00695405226c44198db7650761be1
Sparky <sparky0904@gmail.com>
2014-06-27 11:53:53 +0100

    Class for tool sin the project

---
+++ Archiver/clsTools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Archiver
+{
+    class clsTools
+    {
+        public string PublishVersion
+        {
+            get
+            {
+                if (System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed)
+                {
+                    Version ver = System.Deployment.Application.ApplicationDeployment.CurrentDeployment.CurrentVersion;
+                    return string.Format("{0}.{1}.{2}.{3}", ver.Major, ver.Minor, ver.Build, ver.Revision);
+                }
+                else
+                    return "Not Published";
+            }
+        }
+    }
+}
